04ca95cfe04bfaa60b02b9ad5bcd1caa93d79103
aihongch <99079089+aihongch@users.noreply.github.com>
2022-11-23 02:29:32 -0800

    Add files via upload

--- program/main.sql
+++ program/main.sql
@@ -1,7 +1,6 @@
-
 -- create
 CREATE TABLE METADATA (
-  id INTEGER NOT NULL,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   file_type TEXT NOT NULL,
   file_name TEXT,
   has_child BOOLEAN NOT NULL
@@ -18,11 +17,11 @@ CREATE TABLE SOURCE(
 );
 
 -- insert
-INSERT INTO METADATA (id, file_type, file_name, has_child) VALUES (1,'DICTECTORY','', TRUE);
-INSERT INTO METADATA (id, file_type, file_name, has_child) VALUES (2,'DICTECTORY','user', TRUE);
-INSERT INTO METADATA (id, file_type, file_name, has_child) VALUES (3,'DICTECTORY', 'ellachen', TRUE);
-INSERT INTO METADATA (id, file_type, file_name, has_child) VALUES (4,'DICTECTORY', 'testdic', False);
-INSERT INTO METADATA (id, file_type, file_name, has_child) VALUES (5,'DICTECTORY', '7777777', False);
+INSERT INTO METADATA (file_type, file_name, has_child) VALUES ('DICTECTORY','', TRUE);
+INSERT INTO METADATA (file_type, file_name, has_child) VALUES ('DICTECTORY','user', TRUE);
+INSERT INTO METADATA (file_type, file_name, has_child) VALUES ('DICTECTORY', 'ellachen', TRUE);
+INSERT INTO METADATA (file_type, file_name, has_child) VALUES ('DICTECTORY', 'testdic', False);
+INSERT INTO METADATA (file_type, file_name, has_child) VALUES ('DICTECTORY', '7777777', False);
 
 -- /user/ellachen/testdic
 -- 0/ 1  / 2      /  3
@@ -33,18 +32,11 @@ INSERT INTO PARENT_CHILD VALUES(1, '2');
 INSERT INTO PARENT_CHILD VALUES(2, '3');
 INSERT INTO PARENT_CHILD VALUES(3, '');
 -- file only
---INSERT INTO SOURCE VALUES(1, '1,2');
+INSERT INTO SOURCE VALUES(1, '1,2');
 -- fetch 
 SELECT * FROM METADATA;
 SELECT * FROM PARENT_CHILD;
 SELECT * FROM SOURCE;
 
--- CREATE TABLE METADATA (
---     id INT AUTO_INCREMENT,
---     file_type TEXT NOT NULL,
---     has_child boolean NOT NULL;
---     );
-    
--- INSER INTO METADATA VALUES （1， ‘DICTECTORY’，true）
+-- SELECT * FROM METADATA WHERE id=1;
 
--- SELECT * FROM METADATA;
